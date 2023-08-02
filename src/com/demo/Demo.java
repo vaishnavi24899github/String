af26feceaa8e3296253538dc107ada47cef9178f
vaishnavi <vaishanvi.gulhane1999@gmail.com>
2023-08-02 17:04:09 +0530

    added new code arraylist

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.demo;
+import java.util.ArrayList;
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		
+		al.add("vaishnavi");
+		al.add(null);
+		al.add(null);
+		System.out.println(al);
+	}
+
+}
